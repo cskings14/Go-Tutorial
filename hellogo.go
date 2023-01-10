0a0cd38a00071a3d214197b5663d0981e852dbdb
cskings14 <cskings14@gmail.com>
2023-01-09 23:45:25 -0500

    added more comments

--- hellogo.go
+++ hellogo.go
@@ -2,37 +2,21 @@ package main
 
 import (
 	"fmt" // this is format
-	// 	"bufio"
-	// 	"os"
-	"strings"
+
+	"strings" // there is a seperate strings package
 ) // this is how we import packages into go
 
 var print = fmt.Println // we can instantiate functions as variables to make it easier to use them
 var scan = fmt.Scanln   // this is the scanner
 
 func main() {
-	// 	print("What is your name")
-	// 	value := bufio.NewReader(os.Stdin)
-	// 	name, err := value.ReadString('\n')
-	// 	if err != nil { // if the error has a value, print error. Otherwise, it should print the name. Nil is zero values for many types.
-	// 		print("error")
-	// 	} else { // has to be on the same line as the other curly brace or it will show an error
-	// 		print("Your name is " + name)
-	// 	}
-	// 	if name == "Christian" {
-	// 		print("Hey, that's my name")
-	// 	} else if (string(name[0]) == "C") && (strings.Compare(name, "Christian") != 1){
-	// 		print("We do not have the same name but we share the same first letter")
-	// 	}else {
-	// 		print("Your name is not my name")
-	// 	}
 	print("What is your name")
 	var name string // this will make the variable name a string
 	n, err := scan(&name)
-	if err != nil || n == 0 {
+	if err != nil || n == 0 { // sees if there is an error or if there are no read in values 
 		print("Invalid name")
 	}else {
-	name = strings.ToLower(name)
+	name = strings.ToLower(name) // makes the name lowercase before conditionals
 	if name == "christian" {
 		print("We have the same name")
 	} else if (string(name[0]) == "c") && (name != "christian") {
